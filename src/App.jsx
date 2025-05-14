953becb0656ba4e351ed46de9bdd74da21283d38
fasil <fasilsebsibe27@gmail.com>
2025-05-14 16:41:46 +0300

    fix routes and add package

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+
 
 const app = ()=>{
  return <div>
     <Routes>
-        <Route path="/" element={<SignupPage />} />
-        <Route path="/about" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
     </Routes>
  </div>
-        
 }
 export default app;
+        
+        
