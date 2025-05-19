25573cc58cb2def4244f2750099087dbe6493887
fasil <fasilsebsibe27@gmail.com>
2025-05-19 13:52:57 +0300

    import login and signup files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
 
 const app = ()=>{
  return <div>
     <Routes>
-        <Route path="/" element={<SignupPage />} />
-        <Route path="/about" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
     </Routes>
  </div>
         
