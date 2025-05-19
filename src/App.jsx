4b15b735bd3b2ef62dc06e07ce16f55bf11592f5
fasil <fasilsebsibe27@gmail.com>
2025-05-19 23:08:04 +0300

    import home page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 
 const app = ()=>{
