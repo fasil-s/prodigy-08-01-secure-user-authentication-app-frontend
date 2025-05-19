61c48a6b7e7e88a3530bcbc649741e7b01033ee8
fasil <fasilsebsibe27@gmail.com>
2025-05-19 21:43:36 +0300

    adding home route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LoginPage from "./pages/LoginPage";
 const app = ()=>{
  return <div>
     <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login " element={<LoginPage />} />
     </Routes>
