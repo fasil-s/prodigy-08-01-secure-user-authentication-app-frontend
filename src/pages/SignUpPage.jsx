af791717c86a57dc86aa09d0b7af724ffa6a6724
fasil <fasilsebsibe27@gmail.com>
2025-05-26 16:48:57 +0300

    final test

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,18 +1,34 @@
 import { Eye, EyeOff, Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import toast from "react-hot-toast";
 
 export default function SignUpPage() {
   const [formData, setFormData] = useState({
-    username: "",
+    fullName: "",
     email: "",
     password: "",
   });
   const [showPassword, setShowPassword] = useState(false);
 
+  const { signup, isSigningUp } = useAuthStore();
+  const validateForm = () => {
+    if (!formData.fullName?.trim()) return toast.error("Full name is required");
+    if (!formData.email?.trim()) return toast.error("Email is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email))
+      return toast.error("Invalid email format");
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6)
+      return toast.error("Password must be at least 6 characters");
+
+    return true;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const success = validateForm();
 
-    console.log("Form submitted:", formData);
+    if (success === true) signup(formData);
   };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 via-white to-purple-300 px-4">
@@ -32,12 +48,12 @@ export default function SignUpPage() {
             <User className="text-gray-400 w-5 h-5 mr-2" />
             <input
               type="text"
-              id="username"
+              id="fullName"
               className="w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none bg-transparent"
               placeholder="Username"
-              value={formData.username}
+              value={formData.fullName}
               onChange={(e) =>
-                setFormData({ ...formData, username: e.target.value })
+                setFormData({ ...formData, fullName: e.target.value })
               }
             />
           </div>
